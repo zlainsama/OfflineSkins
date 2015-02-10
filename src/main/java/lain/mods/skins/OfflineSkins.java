0d0f6fa1734ad461a6fd997be609d0583f9562d7
Lain MultipleInstances <zlainsama@163.com>
2015-02-11 05:02:56 +0800

    use a limited pool instead
    add option to not use Crafatar

--- src/main/java/lain/mods/skins/OfflineSkins.java
+++ src/main/java/lain/mods/skins/OfflineSkins.java
@@ -14,6 +14,7 @@ import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StringUtils;
+import net.minecraftforge.common.config.Configuration;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
@@ -131,7 +132,7 @@ public class OfflineSkins extends DummyModContainer
         ModMetadata meta = getMetadata();
         meta.modId = "OfflineSkins";
         meta.name = "OfflineSkins";
-        meta.version = "1.7.10-v2";
+        meta.version = "1.7.10-v3";
         meta.authorList = Arrays.asList("zlainsama");
         meta.description = "made it possible to cache your skins/capes for offline use";
         meta.credits = "";
@@ -184,35 +185,37 @@ public class OfflineSkins extends DummyModContainer
                 }
 
             });
-            images.addSupplier(new ImageSupplier()
-            {
-
-                @Override
-                public BufferedImage loadImage(String name)
+            Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+            if (config.get(Configuration.CATEGORY_GENERAL, "useCrafatar", true).getBoolean(true))
+                images.addSupplier(new ImageSupplier()
                 {
-                    try
-                    {
-                        if (name.startsWith("skins/") && name.endsWith(".png"))
-                        {
-                            name = name.substring(6, name.length() - 4);
-                            if (name.startsWith("uuid/"))
-                                name = name.substring(5);
-                            BufferedImage result = ImageIO.read(new URL("https://crafatar.com/skins/" + name));
-                            if (result.getWidth() != 64 || (result.getHeight() != 64 && result.getHeight() != 32))
-                                return null;
-                            if (result.getHeight() == 64)
-                                result = new LegacyConversion().convert(result);
-                            return result;
-                        }
-                        return null;
-                    }
-                    catch (IOException ignored)
-                    {
-                        return null;
-                    }
-                }
 
-            });
+                    @Override
+                    public BufferedImage loadImage(String name)
+                    {
+                        try
+                        {
+                            if (name.startsWith("skins/") && name.endsWith(".png"))
+                            {
+                                name = name.substring(6, name.length() - 4);
+                                if (name.startsWith("uuid/"))
+                                    name = name.substring(5);
+                                BufferedImage result = ImageIO.read(new URL("https://crafatar.com/skins/" + name));
+                                if (result.getWidth() != 64 || (result.getHeight() != 64 && result.getHeight() != 32))
+                                    return null;
+                                if (result.getHeight() == 64)
+                                    result = new LegacyConversion().convert(result);
+                                return result;
+                            }
+                            return null;
+                        }
+                        catch (IOException ignored)
+                        {
+                            return null;
+                        }
+                    }
+
+                });
         }
     }
 
