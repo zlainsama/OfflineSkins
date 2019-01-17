400504485cf824234cd43a421b504946075a1491
Lain MultipleInstances <zlainsama@163.com>
2019-01-17 16:26:16 +0800

    changes

--- src/main/java/lain/mods/skins/impl/PlayerProfile.java
+++ src/main/java/lain/mods/skins/impl/PlayerProfile.java
@@ -8,37 +8,53 @@ import lain.mods.skins.api.interfaces.IPlayerProfile;
 public class PlayerProfile implements IPlayerProfile
 {
 
-    private WeakReference<GameProfile> p;
+    public static final UUID DUMMY = UUID.fromString("ae9460f5-bf72-468e-89b6-4eead59001ad");
+
+    private WeakReference<GameProfile> _profile;
 
     public PlayerProfile(GameProfile profile)
     {
-        p = new WeakReference<GameProfile>(profile);
+        if (profile == null)
+            throw new IllegalArgumentException("profile must not be null.");
+        _profile = new WeakReference<GameProfile>(profile);
     }
 
     @Override
     public boolean equals(Object o)
     {
+        GameProfile p;
+        if ((p = _profile.get()) == null) // gc
+            return false;
         if (o instanceof PlayerProfile)
-            return p.get().equals(((PlayerProfile) o).p.get());
+            return p.equals(((PlayerProfile) o)._profile.get());
         return false;
     }
 
     @Override
     public UUID getPlayerID()
     {
-        return p.get().getId();
+        GameProfile p;
+        if ((p = _profile.get()) == null) // gc
+            return DUMMY;
+        return p.getId();
     }
 
     @Override
     public String getPlayerName()
     {
-        return p.get().getName();
+        GameProfile p;
+        if ((p = _profile.get()) == null) // gc
+            return "";
+        return p.getName();
     }
 
     @Override
     public int hashCode()
     {
-        return p.get().hashCode();
+        GameProfile p;
+        if ((p = _profile.get()) == null) // gc
+            return 0;
+        return p.hashCode();
     }
 
 }
