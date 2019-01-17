af7dbacfb5e0f8cdedff7b66fedac476c5016495
Lain MultipleInstances <zlainsama@163.com>
2019-01-17 20:22:51 +0800

    changes

--- src/main/java/lain/mods/skins/init/fabric/mixins/ScoreboardHudMixin.java
+++ src/main/java/lain/mods/skins/init/fabric/mixins/ScoreboardHudMixin.java
@@ -1,5 +1,6 @@
 package lain.mods.skins.init.fabric.mixins;
 
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -10,7 +11,7 @@ import net.minecraft.client.gui.hud.ScoreboardHud;
 public abstract class ScoreboardHudMixin extends Drawable
 {
 
-    @ModifyVariable(method = "method_1919(ILnet/minecraft/scoreboard/Scoreboard;Lnet/minecraft/scoreboard/ScoreboardObjective;)V", at = @At(value = "lain.mods.skins.init.fabric.mixins.IStoreInsnPoint", ordinal = 12))
+    @ModifyVariable(method = "method_1919(ILnet/minecraft/scoreboard/Scoreboard;Lnet/minecraft/scoreboard/ScoreboardObjective;)V", at = @At(value = "STORE", opcode=Opcodes.ISTORE, ordinal = 0))
     private boolean onDrawSetFlag_nBXjeY(boolean result)
     {
         return true;
