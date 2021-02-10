26c592db0fff981ca1602a27004be3652d98a38d
Lain MultipleInstances <zlainsama@163.com>
2021-02-10 20:20:42 +0800

    register DISPLAYTEST as client-only

--- src/main/java/lain/mods/skins/init/forge/ForgeOfflineSkins.java
+++ src/main/java/lain/mods/skins/init/forge/ForgeOfflineSkins.java
@@ -1,13 +1,18 @@
 package lain.mods.skins.init.forge;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("offlineskins")
 public class ForgeOfflineSkins {
 
     public ForgeOfflineSkins() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> "client-only", (v, n) -> n));
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
     }
 
