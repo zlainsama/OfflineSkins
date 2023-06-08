b831d9bb9c7dfdc1ed621d51624bd0cc3ecf2f1b
Lain MultipleInstances <zlainsama@163.com>
2023-06-08 18:42:31 +0800

    update to 1.20 fabric

--- src/main/java/lain/mods/skins/init/fabric/mixins/PlayerListHudMixin.java
+++ src/main/java/lain/mods/skins/init/fabric/mixins/PlayerListHudMixin.java
@@ -1,6 +1,5 @@
 package lain.mods.skins.init.fabric.mixins;
 
-import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,9 +7,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerListHud.class)
-public abstract class PlayerListHudMixin extends DrawableHelper {
+public abstract class PlayerListHudMixin {
 
-    @ModifyVariable(method = "render(Lnet/minecraft/client/util/math/MatrixStack;ILnet/minecraft/scoreboard/Scoreboard;Lnet/minecraft/scoreboard/ScoreboardObjective;)V", at = @At(value = "STORE", opcode = Opcodes.ISTORE, ordinal = 0), require = 0)
+    @ModifyVariable(method = "render(Lnet/minecraft/client/gui/DrawContext;ILnet/minecraft/scoreboard/Scoreboard;Lnet/minecraft/scoreboard/ScoreboardObjective;)V", at = @At(value = "STORE", opcode = Opcodes.ISTORE, ordinal = 0), require = 0)
     private boolean onRenderSetFlag_nBXjeY(boolean result) {
         return true;
     }
