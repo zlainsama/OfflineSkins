7e05f13feef98f7280285525e5b6ba42574026fc
Lain MultipleInstances <zlainsama@163.com>
2023-06-09 15:47:36 +0800

    Remove redundant code

--- src/main/java/lain/mods/skins/init/forge/ForgeOfflineSkins.java
+++ src/main/java/lain/mods/skins/init/forge/ForgeOfflineSkins.java
@@ -1,7 +1,5 @@
 package lain.mods.skins.init.forge;
 
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -10,7 +8,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ForgeOfflineSkins {
 
     public ForgeOfflineSkins() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "client-only", (v, n) -> n));
+//        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "client-only", (v, n) -> n));
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
     }
