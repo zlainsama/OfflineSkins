44add7c101ff12cc2410c3d003a7e1596149cf19
Lain MultipleInstances <zlainsama@live.com>
2024-04-24 14:47:44 +0800

    update to 1.20.5 fabric

--- src/main/java/lain/mods/skins/init/fabric/mixins/SkullBlockEntityRendererMixin.java
+++ src/main/java/lain/mods/skins/init/fabric/mixins/SkullBlockEntityRendererMixin.java
@@ -1,10 +1,10 @@
 package lain.mods.skins.init.fabric.mixins;
 
-import com.mojang.authlib.GameProfile;
 import lain.mods.skins.init.fabric.FabricOfflineSkins;
 import net.minecraft.block.SkullBlock;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.block.entity.SkullBlockEntityRenderer;
+import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,10 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(SkullBlockEntityRenderer.class)
 public abstract class SkullBlockEntityRendererMixin {
 
-    @Inject(method = "getRenderLayer(Lnet/minecraft/block/SkullBlock$SkullType;Lcom/mojang/authlib/GameProfile;)Lnet/minecraft/client/render/RenderLayer;", at = @At("RETURN"), cancellable = true, require = 0)
-    private static void getRenderLayer_nBXjeY(SkullBlock.SkullType type, GameProfile profile, CallbackInfoReturnable<RenderLayer> info) {
-        if (type == SkullBlock.Type.PLAYER && profile != null) {
-            Identifier loc = FabricOfflineSkins.getLocationSkin(profile, null);
+    @Inject(method = "getRenderLayer(Lnet/minecraft/block/SkullBlock$SkullType;Lnet/minecraft/component/type/ProfileComponent;)Lnet/minecraft/client/render/RenderLayer;", at = @At("RETURN"), cancellable = true, require = 0)
+    private static void getRenderLayer_nBXjeY(SkullBlock.SkullType type, ProfileComponent profile, CallbackInfoReturnable<RenderLayer> info) {
+        if (type == SkullBlock.Type.PLAYER && profile != null && profile.gameProfile() != null) {
+            Identifier loc = FabricOfflineSkins.getLocationSkin(profile.gameProfile(), null);
             if (loc != null)
                 info.setReturnValue(RenderLayer.getEntityTranslucent(loc));
         }
